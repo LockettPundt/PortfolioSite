bc139caf02c3211dc270bfc6d06f850c322e302a
Lockett Pundt <kalifornium@hotmail.com>
2020-02-24 20:52:06 -0500

    tried to make the autoscroll work but it's maybe a jQuery job.

--- SCRIPTS/LPIV.js
+++ SCRIPTS/LPIV.js
@@ -1,1 +1,9 @@
 'use strict'
+const downArrow = document.getElementById("downArrow");
+// downArrow.addEventListener('click', (e) => {
+//     alert('HI');
+// })
+downArrow.addEventListener('click', e => {
+    let target = document.getElementById("targetScroll");
+    target.scrollIntoView({behavior: 'smooth'})
+});
