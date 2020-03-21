7251ed0170053813357a7629ef4789d780556c5c
Lockett Pundt <kalifornium@hotmail.com>
2020-03-21 19:12:44 -0400

    styled js with eslint

--- SCRIPTS/LPIV.js
+++ SCRIPTS/LPIV.js
@@ -2,9 +2,7 @@ const anchor = document.getElementById('anchor1');
 
 const arrows = document.querySelectorAll('#scrollArrow');
 
-arrows.forEach((item) => {
-  return item.addEventListener('click', (e) => {
-    e.preventDefault();
-    anchor.scrollIntoView(true);
-  })
-});
+arrows.forEach((item) => item.addEventListener('click', (e) => {
+  e.preventDefault();
+  anchor.scrollIntoView(true);
+}));
